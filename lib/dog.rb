75949f363bfc1e410e263a3b276f8c656ddb8a97
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-08 15:32:04 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+  def initialize
+
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def name 
+    return @name
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  
+  def breed 
+    return @breed
+  end
+  
+end
